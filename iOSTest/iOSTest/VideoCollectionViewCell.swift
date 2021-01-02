370bdcd41ae529960ad8526649f9852f76dea04c
Luisa Cruz <luisa.cruzmolina@protonmail.com>
2021-01-02 23:13:09 +0100

    remove async from cell configuration

--- iOSTest/iOSTest/VideoCollectionViewCell.swift
+++ iOSTest/iOSTest/VideoCollectionViewCell.swift
@@ -1,28 +1,26 @@
 import UIKit
 
 final class VideoCollectionViewCell: UICollectionViewCell {
-
+    
     @IBOutlet private weak var thumbnailImageView: UIImageView!
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var subtitleLabel: UILabel!
     @IBOutlet private weak var descriptionLabel: UILabel!
     @IBOutlet private weak var hdImageView: UIImageView!
-
+    
     var video: Video? {
         didSet {
             guard let video = video else {
                 return
             }
-            DispatchQueue.global().async { [thumbnailImageView] in
-                if
-                    let data = try? Data(contentsOf: video.thumbnailURL),
-                    let image = UIImage(data: data)
-                {
-                    DispatchQueue.main.async {
-                        thumbnailImageView?.image = image
-                    }
-                }
+            // It needs to be a sync process, cant use in this stage an async process, it leads to unexpected results
+            // It was enough to remove it, since it was not necesary at all.
+            // we could have a placeholder for the waiting images, that can also help for the videos without image
+            if let data = try? Data(contentsOf: video.thumbnailURL),
+               let image = UIImage(data: data) {
+                thumbnailImageView.image = image
             }
+            
             titleLabel.text = video.title
             subtitleLabel.text = video.subtitle
             descriptionLabel.attributedText = video.attributedDescription
